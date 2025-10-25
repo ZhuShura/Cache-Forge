031af1e57e3c2e419a7f8d75cdbb0ba380be902e
huangtaiji <2460746539@qq.com>
2025-10-26 00:12:42 +0800

    build(整个项目): 构建基本项目结构和启动类
    [skip ci]

---
+++ src/main/java/fun.redis.cacheforge/CacheForgeApplication.java
@@ -0,0 +1,14 @@
+import util.WordArtUtil;
+
+/**
+ * 服务启动器
+ * @author huangtaiji
+ * @date 2025/10/25
+ */
+public class CacheForgeApplication {
+	public static void main(String[] args) {
+		WordArtUtil.wordArtDraw();
+
+
+	}
+}
