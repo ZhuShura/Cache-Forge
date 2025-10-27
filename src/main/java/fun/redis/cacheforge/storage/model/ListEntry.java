76bb24c4baec51da6ed0062eb2c7857545b4d4da
huangtaiji <2460746539@qq.com>
2025-10-27 23:48:46 +0800

    perf: 建立list模板
    [skip ci]

---
+++ src/main/java/fun/redis/cacheforge/storage/model/ListEntry.java
@@ -0,0 +1,19 @@
+package fun.redis.cacheforge.storage.model;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.util.List;
+
+/**
+ * CacheForge列表实体
+ * @author huangtaiji
+ * @date 2025/10/27
+ */
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class ListEntry {
+	private List<String> value;
+}
