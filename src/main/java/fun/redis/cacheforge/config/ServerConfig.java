6f2ffc370ee9dd1f050f1a2fe39e00e2f0946f7b
hua <1026286218@qq.com>
2025-10-28 12:14:08 +0800

    perf:
    [skip ci]

--- src/main/java/fun/redis/cacheforge/config/ServerConfig.java
+++ src/main/java/fun/redis/cacheforge/config/ServerConfig.java
@@ -2,7 +2,6 @@ package fun.redis.cacheforge.config;
 
 import fun.redis.cacheforge.command.CommandRegistry;
 import fun.redis.cacheforge.command.handler.impl.CommandCommandHandler;
-import fun.redis.cacheforge.command.handler.impl.DelCommandHandler;
 
 public class ServerConfig {
     public final int PORT = 6379;
@@ -10,7 +9,7 @@ public class ServerConfig {
     public ServerConfig() {
         // ---------------------Normal-------------------
         CommandRegistry.register("command", new CommandCommandHandler());
-        CommandRegistry.register("del", new DelCommandHandler());
+//        CommandRegistry.register("del", new DelCommandHandler());
         // ---------------------String-------------------
 //        CommandRegistry.register("set", new SetCommandHandler());
 //        CommandRegistry.register("get", new GetCommandHandler());
